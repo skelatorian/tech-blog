ac7703fae27fe9c820d70657cef2b2584758b8cf
Skelatorian <mylynhle21@gmail.com>
2021-02-08 01:15:33 -0500

    come back, error:

--- utils/helpers.js
+++ utils/helpers.js
@@ -17,3 +17,20 @@ module.exports = {
         return word;
     }
   }
+
+ // format_url: url => {
+    return url
+      .replace('http://', '')
+      .replace('https://', '')
+      .replace('www.', '')
+      .split('/')[0]
+      .split('?')[0];
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  }
+};
